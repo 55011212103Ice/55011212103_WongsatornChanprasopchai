e58cc19939c900c3d04639f1f60a966df30d680a
55011212103Ice <student@tests-iMac-131.local>
2014-08-15 16:25:47 +0700

    update
    update_swiftTutorial_1

--- swiftTutorial_1.playground/section-1.swift
+++ swiftTutorial_1.playground/section-1.swift
@@ -1,3 +1,20 @@
 let tutorialTeam = 56
 let editorialTeam = 23
-let totalTeam = tutorialTeam + editorialTeam
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+
+let priceIntInferred = -19
+let priceIntExpLicit = -19
+
+let priceDoubleInterred = -19.99
+let priceDoubleExpLicit:Double = -19.99
+
+let priceFloatInterred = -19.99
+let priceFloatExpLicit:Float = -19.99
+
+let onSaleInterred = true
+let onSaleExpLicit = false
+
+let nameInterred = "Whoopie Cushion"
+let nameExpLicit:String = "Whoopie Cushion"
