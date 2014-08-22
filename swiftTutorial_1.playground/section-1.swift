ff58a136b34c11c3dca672781d295d8830f904ce
55011212028Supakiat <imacatMSU>
2014-08-22 13:58:42 +0700

    add array

--- swiftTutorial_1.playground/section-1.swift
+++ swiftTutorial_1.playground/section-1.swift
@@ -18,3 +18,19 @@ let onSaleExpLicit = false
 
 let nameInterred = "Whoopie Cushion"
 let nameExpLicit:String = "Whoopie Cushion"
+
+
+//Array
+var shoppingListExplicit = [String]()
+shoppingListExplicit = ["Eggs", "Milk"]
+//var shoppingListExplicit:[String] = ["Eggs", "Milk"]
+
+var shoppingList = ["Eggs", "Milk"]
+println("The shopping list contains \(shoppingList.count)items.")
+shoppingList.append("Flour")
+shoppingList += "Banking Powder"
+shoppingList += ["chocolate Spread", "Cheese", "Butter"]
+var firstItem = shoppingList[0]
+shoppingList[0] = "Six eggs"
+//shoppingList[0...3] = ["Bananas", "Apples"]
+shoppingList
