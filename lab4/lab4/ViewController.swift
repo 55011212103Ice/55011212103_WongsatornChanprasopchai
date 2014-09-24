cee5b6132795036085d3d574779dad9317934adb
55011212103Ice <student@tests-iMac-131.local>
2014-09-24 14:59:47 +0700

    lab4

---
+++ lab4/lab4/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  lab4
+//
+//  Created by student on 9/24/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
