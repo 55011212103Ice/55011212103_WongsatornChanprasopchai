62c6bec8d1762c8c2258836f9e2272cf515b259b
student <ralintorn.gle@gmail.com>
2014-10-17 15:17:00 +0700

    segue
    17/10/2014

---
+++ examSegue/examSegue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  examSegue
+//
+//  Created by student on 10/17/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var colorLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+
